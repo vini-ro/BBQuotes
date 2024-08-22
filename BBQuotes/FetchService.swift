1c4124d9d68c5dd3048c9558a6dd81becb6416e8
Vinicius <vroliveira.eng@gmail.com>
2024-08-22 15:56:33 -0300

    Fetch functions Commit

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -19,12 +19,63 @@ struct FetchService {
         let quoteURL = baseURL.appending(path: "quotes/ramdom")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
         
-        // Fetch data
+        // Fetch data Quote
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
         // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw FetchError.badResponse
+        }
         
         // Decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
         
         // Return quote
+        return quote
+    }
+    
+    func fetchCharacter(_ name: String) async throws -> Character {
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        // Fetch data Character
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Character].self, from: data)
+        
+        return characters [0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        // Fetch data Death
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
     }
 }
